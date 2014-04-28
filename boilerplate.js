ad336c406ed109a6065c8557af05940add69c0ec
Kris Reeves <krisreeves@searchfanatics.com>
2014-04-28 18:27:06 +0000

    .

---
+++ boilerplate.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = function (deps) {
+    var Promise = deps.promise,
+        knex = deps.knex,
+        log = deps.log;
+    
+    return {
+        up: function () {
+            
+        },
+        recover: function () {
+            
+        },
+        down: function () {
+            
+        }
+    };
+};
